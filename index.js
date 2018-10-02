993df7fe6a5376ab464373d1f0fad56b0dd25bd5
Keshawn.keaton@gmail.com <keshawn.keaton@gmail.com>
2018-10-02 15:20:12 -0500

    Added views folder,Added pug template, Added sass'

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+//Folder for view files
+app.set('views','./views')
+//Templating engine set to pug
+app.set('view engine','pug')
 app.get('/', (req , res) => res.send('Hello World'))
 app.get(`/about`, (req ,res)=> res.send("This is about the section"))
 app.get(`/page1`, (req,res)=> res.send("Welcome to page 1"))
+app.get('/home', (req,res)=>
+res.render('home',{tittle:"Home", message:"Welcome to the home page"}))
 app.listen(port, () => console.log(`Example app listing on port ${port}!`))
